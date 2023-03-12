ba03c2181d8af80ae31b0b20c7b8891f72b94e31
Tatu Pesonen <tatu@narigon.dev>
2023-03-13 00:52:40 +0200

    feat: remove old-static

--- ui/src/components/NewPasteContent.tsx
+++ ui/src/components/NewPasteContent.tsx
@@ -1,5 +1,4 @@
 import { useEffect } from 'react';
-import { useLoaderData } from 'react-router-dom';
 
 type NewPasteContentProps = {
 	content: string,
@@ -7,7 +6,6 @@ type NewPasteContentProps = {
 }
 
 export const NewPasteContent = (props: NewPasteContentProps) => {
-	const paste = useLoaderData();
 	return (
 		<textarea className="mt-5 flex-1 px-5 bg-transparent font-mono h-100 w-screen border-0 resize-none border-0 outline-none"
       value={props.content} // ...force the input's value to match the state variable...
